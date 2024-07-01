bd544349875647f18ec1cd8d2aac1463c3c50149
szlima <suzanelima@outlook.com>
2024-07-01 11:50:50 -0300

    Criação da função que calcula a diferença entre datas

--- scripts/index.js
+++ scripts/index.js
@@ -26,11 +26,11 @@ function calculateTime(){
     const chosenDate= getChosenDate(dateForm.value);
 
     if(checkChosenDate(chosenDate)){
-        const difference= chosenDate.getTime() - today.getTime();
+        const difference= chosenDate - today;
 
-        if(difference>0)
-            showResultFuture();
-        else if(difference<0)
+        if(difference > 0)
+            showResultFuture(today, chosenDate);
+        else if(difference < 0)
             showResultPast();
         else
             showResultToday(chosenDate);
@@ -70,8 +70,9 @@ function showAlert(){
     }, 4000);    
 }
 
-function showResultFuture(){
-    console.log('Future');
+function showResultFuture(today, chosenDate){
+    const {years, months, days}= getDifferenceDates(today, chosenDate);
+    console.log({years, months, days});
 }
 
 function showResultPast(){
@@ -99,4 +100,58 @@ function getTodayDate(){
         day= '0'+day;
 
     return getChosenDate(`${year}-${month}-${day}`);
+}
+
+const getSizeMonth= (month, year) => {
+    switch(month){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+        case 2:
+            return (year%400 == 0) || (year%4 == 0 && year%100 != 0) ? 29 : 28;
+    }
+};
+
+function getDifferenceDates(date1, date2){
+    const year1= date1.getFullYear(),
+        year2= date2.getFullYear(),
+        month1= date1.getMonth()+1,
+        month2= date2.getMonth()+1,
+        day1= date1.getDate(),
+        day2= date2.getDate();
+
+    let years= year2 - year1,
+        months, days;
+
+    if(month2 >= month1)
+        months= month2- month1;
+    else{
+        months= (12-month1) + month2;
+        years--;
+    }
+
+    if(day2 >= day1)
+        days= day2 - day1;
+    else{
+        const previousMonth2= (month2 === 1) ? 12 : (month2-1);
+        days= (getSizeMonth(previousMonth2,year2)-day1) + day2;
+
+        if(months === 0){
+            months= 11;
+            years--;
+        }else
+            months--;
+    }
+
+    return {years, months, days};
 }
